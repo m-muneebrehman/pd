d87971a6268adc444c4a10ea6ff373b119cfb1e1
Muhammad Muneeb ur Rehman <Kingmc.chohan@gmail.com>
2024-12-29 17:34:56 +0500

    updating front end

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,21 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Set the pixmap for the title QLabel
+    QPixmap pixmap(":/images/main.png");
+    if (!pixmap.isNull()) {
+        ui->title->setPixmap(pixmap);
+        ui->title->setScaledContents(true); // Ensure the image scales with the label
+    } else {
+        qDebug() << "Failed to load main.png";
+    }
 }
 
 MainWindow::~MainWindow()
@@ -20,4 +29,3 @@ void MainWindow::on_pushButton_clicked()
     secPage->show();
     hide();
 }
-
